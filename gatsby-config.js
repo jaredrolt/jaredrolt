096558eb4e7c25fba4c2896322ef962f28daf352
Jared Rolt <jrolt92@gmail.com>
2020-01-15 23:42:59 +0800

    update home page content

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-typescript',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,6 +26,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sassRuleTest: /\.s(a|c)ss$/,
+        precision: 6,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -34,6 +34,13 @@ function SEO({ description, lang, meta, title }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
+      link={[
+        {
+          href: 'https://fonts.googleapis.com/css?family=Raleway:100,500',
+          rel: 'stylesheet',
+          type: 'text/css'
+        }
+      ]}
       meta={[
         {
           name: `description`,
