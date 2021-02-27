95651902d0d04a25a99fa6e9ca38a1b0486c0f9a
Jared Rolt <jrolt92@gmail.com>
2021-02-27 23:40:32 +0800

    Post login actions

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,43 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import FacebookLogin, { ReactFacebookFailureResponse, ReactFacebookLoginInfo } from 'react-facebook-login';
 
 export const Login = () => {
-  const callback = useCallback((response: ReactFacebookLoginInfo|ReactFacebookFailureResponse) => {
-    console.log(response);
-  }, [])
+  const [user, setUser] = useState('');
+  const callback = useCallback(async (response: ReactFacebookLoginInfo|ReactFacebookFailureResponse) => {
+    if (!('accessToken' in response)) {
+      window.alert('Failed to login with FB');
+      return;
+    }
+
+    await fetch('/api/csrf-cookie');
+    const loginResponse = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        access_token: response.accessToken,
+      }),
+    }).then(response => response.json());
+
+    if (!loginResponse || loginResponse.message !== 'success') {
+      window.alert('Failed to login');
+      return;
+    }
+
+    const userResponse: { name: string } = await fetch('/api/user').then(response => response.json());
+    setUser(userResponse.name);
+  }, []);
+
   return (
     <div>
       <h1>This is the login</h1>
-      <FacebookLogin appId={String(process.env.GATSBY_FACEBOOK_APP_ID)} callback={callback}  />
+      {!user && <FacebookLogin appId={String(process.env.GATSBY_FACEBOOK_APP_ID)} callback={callback}  />}
+      {user && (
+        <div>
+          Welcome, {user} :)
+        </div>
+      )}
     </div>
   );
 };
